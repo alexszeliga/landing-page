38794d38e4762154400fea64fa7b6172c25867cc
alexszeliga <alexszeliga@gmail.com>
2019-07-08 16:28:15 -0400

    basic readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-const { series, src, dest } = require('gulp');
+const { series, src, dest, watch } = require('gulp');
 const babel = require('gulp-babel');
 const sass = require('gulp-sass');
 const prefix = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
 // style paths
 const scssFiles = 'src/scss/*.scss';
@@ -14,20 +15,30 @@ const jsDestination = 'public/javascripts/';
 function scssTask() {
     // place code for scss task here
     return src(scssFiles)
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(prefix())
+        .pipe(sourcemaps.write())
         .pipe(dest(cssDestination));
 }
 
 function jsTask() {
     // place code for js task here
     return src(jsFiles)
+        .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['@babel/env']
         }))
+        .pipe(sourcemaps.write())
         .pipe(dest(jsDestination));
 }
-  
+
+function watchFiles() {
+    watch(scssFiles, scssTask);
+    watch(jsFiles, jsTask);
+}
+
+exports.watch = watchFiles;
 exports.scss = scssTask;
 exports.js = jsTask;
 exports.default = series(scssTask, jsTask);
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
-# Bluespring Landing
+# Bluespring Landing Environment
+
+This repo is a development environment for the BlueSpring Wealth Partners Landing Page.
+
+# Would you like to know more?
+* Express server set up using express-generator
+* Automation and transpilation handled by Gulp
+* * SCSS transpiled to CSS using Sass
+* * JS transpiled to JS using Babel
